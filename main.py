d768b6fbd1eeaf80df6f1c2ae2517de668b3dfb5
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-10-11 13:31:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Client as Bot
 
 from callsmusic import run
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from handlers import __version__
+from KimMusik import __version__
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
